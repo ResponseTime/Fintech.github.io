5c2a9afee8fe77aee41ba26b0b02a563e423833d
Looper <aayush.bhattacharjee2002@gmail.com>
2021-10-28 05:14:53 -0700

    added content to the website and a few animation

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Fintech.github.io
 Developing a website on the topic Fintech for a hackathon
+
+Sources Used
+https://www.investopedia.com/terms/f/fintech.asp
--- js.js
+++ js.js
@@ -1,12 +1,16 @@
 let heading = document.getElementById('heading');
 let content = document.getElementById('content');
+let arrow = document.getElementById('arrow-head');
 content.style.display = 'none';
 heading.addEventListener('click', function(e) {
     if(content.style.display == 'none'){
         content.style.display = 'block';
+        heading.innerHTML = "THIS IS <br>&emsp;&emsp;&emsp; FINTECH"
         content.style.transition = '0.5s ease-in';
+        arrow.style.display = 'none';
     }
     else{
         content.style.display = 'none';
+        heading.innerHTML = "WHAT IS<br>&emsp;&emsp;&emsp;FINTECH&emsp14;?"
     }
 });
